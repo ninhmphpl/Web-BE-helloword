b43f1eec016c09ec51df5fb48e82d4f5c592436a
ADMIN <taiphiets666@gmail.com>
2023-02-12 09:23:03 +0700

    Kết hợp code dev1 với a Linh

--- src/main/java/com/spring/web/controller/admin/EmployeeController.java
+++ src/main/java/com/spring/web/controller/admin/EmployeeController.java
@@ -12,10 +12,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
+import org.springframework.expression.ExpressionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.data.domain.Sort.Direction.DESC;
@@ -32,6 +35,8 @@ public class EmployeeController {
     private PositionService positionService;
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeService employeeServices;
 
     // Hiển thị toàn bộ emplooyee và phân trang
     @GetMapping("/employee-list")
@@ -64,4 +69,55 @@ public class EmployeeController {
         employee.setStatus(new Status(status,null));
         return new ResponseEntity<>(employeeService.save(employee),HttpStatus.OK);
     }
+    // Phương thức tìm kiếm nhân viên
+    @GetMapping("/search")
+    public ResponseEntity<List<Employee>> findSearch (@RequestParam("search") String search){
+        return new ResponseEntity<>(employeeServices.findAllByNameEmployee(search), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<?> save (@RequestBody Employee employee){
+        employeeService.save(employee);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping("employees")
+    public Employee createEmployee (@Valid @RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    // XOá thẳng nhân viên khỏi base
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEmployee (@PathVariable Long id){
+        employeeRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    //    @GetMapping("/{id}")
+//    public ResponseEntity<?> renderUpdate(@PathVariable Long id){
+//        Employee employee = employeeService.findById(id).get();
+//        EmployeeRender employeeRender = employeeService.render(employee);
+//        return new ResponseEntity<>(employeeRender, HttpStatus.OK);
+//    }
+    @PutMapping
+    public ResponseEntity<?> update (@RequestBody Employee employee){
+        employeeService.save(employee);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    // Phương thức update Nhân Viên ( đang giai đoạn kiểm thử)
+    @PutMapping("employees/{id}")
+    public ResponseEntity<Employee> updateEmployee (@PathVariable(value = "id") Long id,
+                                                    @Valid @RequestBody Employee employee) throws Exception {
+        Employee employees = employeeRepository.findById(id)
+                .orElseThrow(() -> new ExpressionException("Nhân viên này không tồn tại: " + id));
+
+        employee.setAge(employee.getAge());
+        employee.setName(employee.getName());
+        employee.setBirth(employee.getBirth());
+        employee.setId(employee.getId());
+        employee.setGender(employee.getGender());
+        employee.setStatus(employee.getStatus());
+        employee.setPhoneNumber(employee.getPhoneNumber());
+        employee.setPosition(employee.getPosition());
+        employee.setUser(employee.getUser());
+        final Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
+
