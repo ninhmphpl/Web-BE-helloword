5f9497193411af1ad20a680006a9e593f282de9a
Do Vu Linh <linhdovu95@gmail.com>
2023-02-17 14:50:06 +0700

    seacrh theo tên product và tên category

--- src/main/java/com/spring/web/controller/product/ProductSimplController.java
+++ src/main/java/com/spring/web/controller/product/ProductSimplController.java
@@ -6,6 +6,7 @@ import com.spring.web.service.impl.CategoryService;
 import com.spring.web.service.impl.ProductSimpleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
@@ -30,13 +31,15 @@ public class ProductSimplController {
     public ResponseEntity<?> searchProductBuyNamOrCategory(@PathVariable("name") String name,
                                                            @PageableDefault(value = 10)
                                                                    @SortDefault(sort = "id",direction = ASC) Pageable pageable) {
-        Page<ProductSimple> page = productSimpleService.findAllPageAndNameContaining(pageable,name);
-        Page<ProductSimple> page1 = productSimpleService.findAllByCategoryName(pageable,name);
-
+        List<ProductSimple> productSimpleList = productSimpleService.findAllByCategoryName("%" + name + "%");
+//       Chuyển đổi từ List sang Page
+        final int start = (int)pageable.getOffset();
+        final int end = Math.min((start + pageable.getPageSize()), productSimpleList.size());
+        final Page<ProductSimple> page = new PageImpl<>(productSimpleList.subList(start, end), pageable, productSimpleList.size());
+//
         if(pageable.getPageNumber() >= page.getTotalPages() || pageable.getPageNumber() < 0){
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
-
-        return new ResponseEntity<>(page1 , HttpStatus.OK);
+        return new ResponseEntity<>(page , HttpStatus.OK);
     }
 }
