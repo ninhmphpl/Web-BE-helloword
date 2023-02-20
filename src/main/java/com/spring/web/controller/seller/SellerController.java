1d33177edbb68c74bd4e5c0ad995c00a60ba9e4d
ninhmphpl <ninhmphpl@gmail.com>
2023-02-20 15:30:12 +0700

    up all , ket thuc spint 2

--- src/main/java/com/spring/web/controller/seller/SellerController.java
+++ src/main/java/com/spring/web/controller/seller/SellerController.java
@@ -1,23 +1,36 @@
 package com.spring.web.controller.seller;
 
+import com.spring.web.model.ProductDetail;
+import com.spring.web.model.ProductSimple;
 import com.spring.web.model.Seller;
 import com.spring.web.model.User;
-import com.spring.web.service.impl.SellerService;
-import com.spring.web.service.impl.UserService;
+import com.spring.web.repository.SellerRepository;
+import com.spring.web.service.impl.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Direction.ASC;
+
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/sigin")
+@RequestMapping("/seller")
 public class SellerController {
     @Autowired
     private SellerService sellerService;
-
     @Autowired
     private UserService userService;
+    @Autowired
+    private ProductDetailService productDetailService;
+    @Autowired
+    private ProductSimpleService productSimpleService;
 
 
     // check User tồn tại hay chưa
@@ -42,6 +55,28 @@ public class SellerController {
         Object seller1 = sellerService.create(seller);
         return new ResponseEntity<>(seller1,HttpStatus.OK);
     }
-//    @PostMapping("/sellet/create-product")
+
+    @GetMapping("/list")
+    public ResponseEntity<?> findAllSellerPage(@PageableDefault(value = 10)
+                                               @SortDefault(sort = "id", direction = ASC) Pageable pageable) {
+        Page<Seller> page = sellerService.findAllSellerPage(pageable);
+        if (pageable.getPageNumber() >= page.getTotalPages() || pageable.getPageNumber() < 0) {
+            System.out.println("Page Number out range page");
+            return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+    @PostMapping("/create-product/{id}")
+    public ResponseEntity<?> create(@RequestBody ProductDetail productDetail,
+                                    @PathVariable Long id){
+        Seller seller1= sellerService.findById(id).get();
+        ProductDetail productDetail1 = productDetailService.createProduct(productDetail);
+        ProductSimple productSimple1 = productSimpleService.findById(productDetail1.getId()).get();
+        List<ProductSimple> productSimpleList = seller1.getListProduct();
+        productSimpleList.add(productSimple1);
+        seller1.setListProduct(productSimpleList);
+        sellerService.save(seller1);
+        return new ResponseEntity<>(seller1, HttpStatus.OK);
+    }
 
 }
